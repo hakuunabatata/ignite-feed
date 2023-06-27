486ca7b782b19f45da47fe66bd13f02630c1dd69
hakuunabatata <emachadomartins1999@gmail.com>
2023-06-26 22:12:51 -0300

    chore: create app module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main></main>
+      </div>
+    </div>
+  );
 }
