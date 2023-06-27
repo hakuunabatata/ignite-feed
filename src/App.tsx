af6baa11e93a156b6381300f15ca71535b77b707
hakuunabatata <emachadomartins1999@gmail.com>
2023-06-26 22:27:10 -0300

    chore: create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Header } from "./components/Header";
 import "./global.css";
 import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <aside>sidebar</aside>
+        <Sidebar />
         <main></main>
       </div>
     </div>
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import styles from "./Sidebar.module.css";
+
+export const Sidebar = () => (
+  <aside className={styles.sidebar}>
+    <img
+      className={styles.cover}
+      src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+    />
+    <div className={styles.profile}>
+      <strong>User</strong>
+      <span>Dev</span>
+    </div>
+
+    <footer>
+      <a href="#">Editar seu perfil</a>
+    </footer>
+  </aside>
+);
