791df0017a2f57f2b1019238f0ba2c8af98fead9
hakuunabatata <emachadomartins1999@gmail.com>
2023-06-26 22:41:26 -0300

    chore: sidebar edit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { PencilSimpleLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export const Sidebar = () => (
@@ -7,12 +8,19 @@ export const Sidebar = () => (
       src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
     />
     <div className={styles.profile}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/hakuunabatata.png"
+      />
       <strong>User</strong>
       <span>Dev</span>
     </div>
 
     <footer>
-      <a href="#">Editar seu perfil</a>
+      <a href="#">
+        <PencilSimpleLine size={20} />
+        Editar seu perfil
+      </a>
     </footer>
   </aside>
 );
