be7528d351a670944263d2d1b4385ddcfa7218d9
hakuunabatata <emachadomartins1999@gmail.com>
2023-06-27 21:46:37 -0300

    chore: create comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export const Comment = () => (
+  <div className={styles.comment}>
+    <img src="https://github.com/hakuunabatata.png" />
+
+    <div className={styles.commentBox}>
+      <div className={styles.commentContent}>
+        <header>
+          <div className={styles.authorAndTime}>
+            <strong>User</strong>
+
+            <time dateTime={new Date().toString()}>Publicado há 1h</time>
+          </div>
+
+          <button title="Deletar comentário">
+            <Trash size={20} />
+          </button>
+        </header>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque fuga
+          nostrum amet cumque aliquam, rerum harum, similique reprehenderit
+          voluptatem doloremque quas. A facere sunt quisquam dignissimos sequi
+          in et quas?
+        </p>
+      </div>
+
+      <footer>
+        <button>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </button>
+      </footer>
+    </div>
+  </div>
+);
